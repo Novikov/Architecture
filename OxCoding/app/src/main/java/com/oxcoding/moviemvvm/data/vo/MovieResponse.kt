45fa184b6bb4ef666b567e08db811029186c8cff
Igor Novikov <novikov.mail@inbox.ru>
2020-12-25 15:55:07 +0300

    save point

--- OxCoding/app/src/main/java/com/oxcoding/moviemvvm/data/vo/MovieResponse.kt
+++ OxCoding/app/src/main/java/com/oxcoding/moviemvvm/data/vo/MovieResponse.kt
@@ -4,7 +4,6 @@ package com.oxcoding.moviemvvm.data.vo
 import com.google.gson.annotations.SerializedName
 
 data class MovieResponse(
-    val page: Int,
     @SerializedName("results")
     val movieList: List<Movie>,
     @SerializedName("total_pages")
