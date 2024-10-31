616e88975ee3b79bb8929fc8d51dee998bb884c4
Novikov <abovefortune@gmail.com>
2024-10-31 15:17:47 +0300

    Plan

---
+++ app/src/main/java/com/app/architecture/Plan.kt
@@ -0,0 +1,11 @@
+package com.app.architecture
+
+/**
+ * Реализовать 3х модульное приложение и рассмотреть основные Presentation паттерны (MVP, MVVM, MVI)
+ * Настроить DI
+ * Рассмотреть основные паттерны для domain и Data слоев. UseCase, Interactor, Repository, DataSource и другие
+ *
+ * В этом же проекте реализовать Feature-module проект
+ * Настроить DI
+ *
+ * */
